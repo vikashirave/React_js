6a955c8f72be711b468359e5e45f3daf4046ffc3
Vikas Hirave <45948809+vikashirave@users.noreply.github.com>
2021-01-09 23:30:05 +0530

    Conditional rendering

--- hello-world/src/components/ChildComponent.js
+++ hello-world/src/components/ChildComponent.js
@@ -4,7 +4,7 @@ function ChildComponent(props) {    //child component
         <div>
             <button onClick={() => props.greetHandler('child')}>Greet Parent</button>
         </div>
-    )
+    
 }
 
 export default ChildComponent
--- hello-world/src/components/UserGreeting.js
+++ hello-world/src/components/UserGreeting.js
@@ -6,17 +6,57 @@ import React, { Component } from 'react'
        super(props)
    
        this.state = {
-            
+            isLoggedIn: true
        }
    }
    
 
     render() {
-        return (
-            <div>
-                Welcome Vikas
-            </div>
-        )
+        //conditinal rendering
+           //4th aproach               //shrt circuit operator
+
+       return this.state.isLoggedIn && <div>welcome Vikas</div> 
+
+        //3rd aproach
+        // return (
+        //     this.state.isLoggedIn ? (
+        //     <div>Welcome Vikas</div>      //conditional operator
+        //     ) : (
+        //     <div>Welcome Guest</div>
+        //     )
+        
+        // )
+        //2nd Aproach
+        // let message
+        // if (this.state.isLoggedIn) {
+        //     message = <div>Welcome Vikas</div>
+        // } else {
+        //     message = <div>Welcome Guest</div>
+        // }
+
+        // return <div>{message}</div>
+
+         //1st Aproach
+        // if(this.state.isLoggedIn){
+        //     return(
+        //         <div>
+        //             Welcome Vikas
+        //         </div>
+        //     )
+        // }
+        // else {
+        //     return(
+        //         <div>
+        //             Welcome Guest
+        //         </div>
+        //     )
+        //}
+        // return (
+        //     <div>
+        //        <div> Welcome Vikas</div>
+        //        <div>welcome Guest</div>
+        //     </div>
+        // )
     }
 }
 
